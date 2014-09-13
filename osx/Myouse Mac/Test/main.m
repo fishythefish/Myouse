46ba0830544ce9df72042139b9ee01d3dffca767
thomasfinch <tomf64@gmail.com>
2014-09-13 00:22:04 -0400

    Added barebones mac app

---
+++ osx/Myouse Mac/Test/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Test
+//
+//  Created by Thomas Finch on 9/12/14.
+//  Copyright (c) 2014 Thomas Finch. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
