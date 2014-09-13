3975fa7a9407232c609a4f9d6e8592513d122847
Andrew Rochman <aokiroch@yahoo.com>
2014-09-12 23:22:05 -0400

    so I can commit

---
+++ garbage.cpp
@@ -0,0 +1,9 @@
+//
+//  garbage.cpp
+//  Myouse
+//
+//  Created by Andrew Rochman on 9/12/14.
+//  Copyright (c) 2014 Andrew Rochman. All rights reserved.
+//
+
+#include "garbage.h"
---
+++ garbage.h
@@ -0,0 +1,14 @@
+//
+//  garbage.h
+//  Myouse
+//
+//  Created by Andrew Rochman on 9/12/14.
+//  Copyright (c) 2014 Andrew Rochman. All rights reserved.
+//
+
+#ifndef __Myouse__garbage__
+#define __Myouse__garbage__
+
+#include <iostream>
+
+#endif /* defined(__Myouse__garbage__) */
